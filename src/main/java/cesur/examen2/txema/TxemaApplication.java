912685b159c9740430ce57419998fc45e78c8040
Txema <josemaria.s317461@cesurformacion.com>
2023-11-27 12:42:43 +0100

    Antes del examen. Creado proyecto y packages

---
+++ src/main/java/cesur/examen2/txema/TxemaApplication.java
@@ -0,0 +1,13 @@
+package cesur.examen2.txema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cesur/examen2/txema/TxemaApplicationTests.java
@@ -0,0 +1,13 @@
+package cesur.examen2.txema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
